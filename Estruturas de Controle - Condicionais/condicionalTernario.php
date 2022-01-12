47c3874d83b00bff4b990a2b57af22d2df139dd7
Aguiar <mateushenriqueaguiar@hotmail.com>
2022-01-12 10:47:44 -0300

    Estudado Condicionais

---
+++ Estruturas de Controle - Condicionais/condicionalTernario.php
@@ -0,0 +1,14 @@
+<?php
+
+
+// condição ? se verdadeiro : se falso
+
+$idade = 18;
+
+$validaMaiorIdade = $idade >= 18 ? 'Maior de idade' : 'Menor de idade';
+
+echo $validaMaiorIdade . PHP_EOL;
+
+$nome = 'Mateus';
+
+echo $nome != 'Mateus' ? 'Seu nome não é Mateus' : 'Seu nome é Mateus';
